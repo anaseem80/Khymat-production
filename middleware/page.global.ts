e646d90b9120a1efcc2ffd4d5f0a402268a3bf9e
Ahmed Naseem <111666236+anaseem80@users.noreply.github.com>
2024-05-16 04:01:03 +0300

    Delete middleware directory

--- middleware/page.global.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
-    if (to.path !== from.path) {
-        window.scrollTo(0, 0)
-    }
-    to.meta.pageTransition = { name: 'page-left' };
-    from.meta.pageTransition = { name: 'page-left' };
-})
